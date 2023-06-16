0ebb37ee1b1e9497c8ea92f344512aafda65e45e
fernando-r-costa <frc_vet@hotmail.com>
2023-06-16 15:33:42 -0300

    Testando Faki API

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3004/users",
+  baseURL: "https://banco-dados.vercel.app/users",
   // json-server --watch db_usuarios.json --port 3004
 });
 
